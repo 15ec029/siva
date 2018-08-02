35dd354e0ef6cee2e2a62c5ec47b8cec5cb18d0f
15ec029 <36722812+15ec029@users.noreply.github.com>
2018-08-02 15:52:41 +0530

    Create eve.java

---
+++ multiple/eve.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class eve
+{
+public static void main(String args[])
+{
+int i,n,sum=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(i=1;i<=5;i++)
+{
+sum=i*n;
+System.out.println(sum);
+}
+}
+}
+
