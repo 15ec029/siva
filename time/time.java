bb459b292d948520d579e2da2025c52d03e29a02
15ec029 <36722812+15ec029@users.noreply.github.com>
2018-08-09 16:30:57 +0530

    Create time.java

---
+++ time/time.java
@@ -0,0 +1,15 @@
+import.java.io*;
+import.java.util.*;
+public class time
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner s=new Scanner();
+b=nextInt();
+a=b/60;
+c=b%60;
+System.out.print(a+" "+b);
+}
+}
+
